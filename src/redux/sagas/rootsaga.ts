90af4ee475bd63cbe3f5478c62c7f3231b6011dd
ShakirHajabba <shakir@emproto.com>
2023-04-24 16:29:34 +0530

    configured husky

--- src/redux/sagas/rootsaga.ts
+++ src/redux/sagas/rootsaga.ts
@@ -1,4 +1,4 @@
-import { all, fork } from 'redux-saga/effects';
+// import { all, fork } from 'redux-saga/effects';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function* rootSaga() {
